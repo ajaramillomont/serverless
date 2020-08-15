f1cdec7f6056e5aaf981a1d9dc41c1194e6789d2
ajaramillomont <ajaramillomont@gmail.com>
2020-08-15 14:00:45 -0500

    nuevos cambios

--- api/routes/meals.js
+++ api/routes/meals.js
@@ -6,4 +6,19 @@ router.get('/', (req, res) =>{
     res.send('hola soy meals')   
 });
 
+router.get('/:id', (req, res) =>{
+    res.send(req.params.id)   
+});
+
+router.post('/', (req, res) =>{
+    res.send('soy post')   
+});
+
+router.put('/', (req, res) =>{
+    res.send('hola soy meals')   
+});
+
+router.delete('/:id', (req, res) =>{
+    res.send('soy delete')   
+});
 module.exports = router;
