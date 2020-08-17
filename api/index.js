2d792ad5d93ac935acba63f479c1637ea8109c1a
ajaramillomont <ajaramillomont@gmail.com>
2020-08-17 08:15:51 -0500

    cambios

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const meals = require('./routes/meals');
 const orders = require('./routes/orders');
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
